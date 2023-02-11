53d30fd10fb3fd136740ee36c057116866356f72
Adrian <adrian.barriga.g@gmail.com>
2023-02-11 09:17:39 -0700

    first commit

---
+++ testJava/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        suma(10, 30);
+
+        System.out.println();
+    }
+
+    public static void suma(int a, int b){
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a - b);
+
+    }
+}
