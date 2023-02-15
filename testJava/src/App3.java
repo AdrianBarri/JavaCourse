1d9a31a014853bff620fedad97cbabeebb7ee736
Adrian <adrian.barriga.g@gmail.com>
2023-02-15 16:34:26 -0700

    commit de metodo

--- testJava/src/App3.java
+++ testJava/src/App3.java
@@ -1,7 +1,23 @@
 public class App3 {
 
     public static void main(String [] args) throws Exception {
+        Potato miPotato = new Potato();
+        miPotato.QuitarBrazo();
+        miPotato.QuitarBrazo();
+        miPotato.QuitarBrazo();
+
+        System.out.println(miPotato.brazos);
         
     }
+
     
+}
+
+class Potato{
+    public int brazos = 0;
+
+    public void QuitarBrazo(){
+        this.brazos--;
+    }
+
 }
