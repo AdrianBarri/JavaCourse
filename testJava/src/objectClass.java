f55825e7e305ff4b5c94036ba01a63b15eaff06a
Adrian <adrian.barriga.g@gmail.com>
2023-02-16 14:36:45 -0700

    cambio de nombre para las clases

---
+++ testJava/src/objectClass.java
@@ -0,0 +1,23 @@
+public class objectClass {
+
+    public static void main(String [] args) throws Exception {
+        Potato miPotato = new Potato();
+        miPotato.QuitarBrazo();
+        miPotato.QuitarBrazo();
+        miPotato.QuitarBrazo();
+
+        System.out.println(miPotato.brazos);
+        
+    }
+
+    
+}
+
+class Potato{
+    public int brazos = 0;
+
+    public void QuitarBrazo(){
+        this.brazos--;
+    }
+
+}
