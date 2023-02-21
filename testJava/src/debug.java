4074047e694bde5f9ac72472aebaf693bd0daa6b
Adrian <adrian.barriga.g@gmail.com>
2023-02-20 18:22:03 -0700

    se agrego la clase de debug

---
+++ testJava/src/debug.java
@@ -0,0 +1,29 @@
+public class debug {
+    public static void main(String [] args){
+        funcion1();
+    }
+
+    public static void funcion1() {
+        funcion2();
+    }
+
+    public static void funcion2() {
+        funcion3();
+    }
+
+    public static void funcion3() {
+        funcion4();
+    }
+
+    public static void funcion4() {
+        funcion5();
+    }
+
+    public static void funcion5() {
+        System.out.println("Holaaaaa");
+        var i = 15;
+
+        System.out.println(15/0);
+    }
+    
+}
