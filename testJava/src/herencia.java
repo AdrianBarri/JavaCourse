07fc3b271d62cc399abf72ef48c0b3011151508d
Adrian <adrian.barriga.g@gmail.com>
2023-03-01 19:02:33 -0700

    commit lase herencia

---
+++ testJava/src/herencia.java
@@ -0,0 +1,17 @@
+public class herencia {
+    
+    public static void main(String [] args) throws Exception{
+
+        
+
+    }
+}
+
+class Vehiculo{
+    int velocidadMaxima;
+    String matricula;
+}
+
+class Carro extends Vehiculo{
+    
+}
